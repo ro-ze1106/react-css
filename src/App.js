b82584465147dfecc9299f7f1ecff9dca4205243
ro-ze1106 <85288297+ro-ze1106@users.noreply.github.com>
2022-05-17 07:06:50 +0900

    CSS適応方法

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import { CssModules } from "./components/CssModules";
 import { InlineStyly } from "./components/InlineStyle";
+import { StyledJsx } from "./components/StyledJsx";
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
       <InlineStyly />
+      <CssModules />
+      <StyledJsx />
     </div>
   );
 }
